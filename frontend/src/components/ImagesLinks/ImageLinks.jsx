0e53b032871b4272b68c222904107e617be139cd
AdrienBlanco <adrienblanco89@gmail.com>
2024-04-24 19:16:28 +0200

    Ajout d'un titre sur la section ImageLinks

--- frontend/src/components/ImagesLinks/ImageLinks.jsx
+++ frontend/src/components/ImagesLinks/ImageLinks.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Highlight from "../Highlight/Highlight"
 import "./ImageLinks.scss";
 
 export default function ImageLinks({ sectionClass, wave, titles, links, src, alt }) {
@@ -7,12 +7,17 @@ export default function ImageLinks({ sectionClass, wave, titles, links, src, alt
   return (
     <section className={waveSection}>
       <div className={`${sectionClass}__container`}>
-        {titles.map((title, index) => (
-          <a className={`${sectionClass}__link`} key={index} href={links[index]}>
-            <h3 className={`${sectionClass}__title`}>{title}</h3>
-            <img className={`${sectionClass}__image`} src={src[index]} alt={alt[index]} />
-          </a>
-        ))}
+        <div className={`${sectionClass}__title title-container`}>
+          <Highlight tag="h2">Découvrez nos Deux Activités</Highlight>
+        </div>
+        <div className={`${sectionClass}__content`}>
+          {titles.map((title, index) => (
+            <a className={`${sectionClass}__link`} key={index} href={links[index]}>
+              <h3 className={`${sectionClass}__link__title`}>{title}</h3>
+              <img className={`${sectionClass}__image`} src={src[index]} alt={alt[index]} />
+            </a>
+          ))}
+        </div>
       </div>
     </section>
   )

0f2cd9849e48d6ac53a179ffe13a34f6b9f05604
AdrienBlanco <adrienblanco89@gmail.com>
2024-04-26 17:37:54 +0200

    feat(Meta): Ajout d'une ligne site_name
    Recommandé pour le partage sur les RS

--- frontend/src/helmet/Meta.jsx
+++ frontend/src/helmet/Meta.jsx
@@ -12,6 +12,7 @@ export default function Meta() {
                 <meta name="twitter:title" content="Ferme de la corne dorée - Chèvrerie et Equitation Western" />
                 <meta name="twitter:description" content="Découvrez la ferme de la corne dorée, production et vente de fromages de chèvre artisanaux et cours d'équitation western." />
                 <meta name="twitter:image" content="./img/logo_de_la_ferme_de_la_corne_doree.jpg" />
+                <meta name="twitter:site_name" content="Ferme de la corne dorée" />
 
                 <meta property="og:title" content="Ferme de la corne dorée - Chèvrerie et Equitation Western" />
                 <meta property="og:type" content="website" />

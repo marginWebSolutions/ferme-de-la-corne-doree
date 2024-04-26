4958bb9bd535f7a5b8ccbed448c028ef3e6ebea7
AdrienBlanco <adrienblanco89@gmail.com>
2024-04-26 17:43:24 +0200

    feat(Meta): Ajout de site_name

--- frontend/src/helmet/Meta.jsx
+++ frontend/src/helmet/Meta.jsx
@@ -12,13 +12,13 @@ export default function Meta() {
                 <meta name="twitter:title" content="Ferme de la corne dorée - Chèvrerie et Equitation Western" />
                 <meta name="twitter:description" content="Découvrez la ferme de la corne dorée, production et vente de fromages de chèvre artisanaux et cours d'équitation western." />
                 <meta name="twitter:image" content="./img/logo_de_la_ferme_de_la_corne_doree.jpg" />
-                <meta name="twitter:site_name" content="Ferme de la corne dorée" />
 
                 <meta property="og:title" content="Ferme de la corne dorée - Chèvrerie et Equitation Western" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://www.lacornedoree.fr" />
                 <meta property="og:description" content="Découvrez la ferme de la corne dorée, production et vente de fromages de chèvre artisanaux et cours d'équitation western." />
                 <meta property="og:image" content="./img/logo_de_la_ferme_de_la_corne_doree.jpg" />
+                <meta property="og:site_name" content="Ferme de la corne dorée" />
             </Helmet>
         </>
     );

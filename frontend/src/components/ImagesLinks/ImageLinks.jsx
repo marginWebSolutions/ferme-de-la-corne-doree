9bd1d36995e5ea9369e47fae969fb607ab4247e7
Naoh <27915369+N40h@users.noreply.github.com>
2024-05-02 11:19:57 +0200

    style[app]: Ajout de style sur l'entièreté du site

--- frontend/src/components/ImagesLinks/ImageLinks.jsx
+++ frontend/src/components/ImagesLinks/ImageLinks.jsx
@@ -1,27 +1,38 @@
-import Highlight from "../Highlight/Highlight"
-import "./ImageLinks.scss";
+import Highlight from '../Highlight/Highlight';
+import './ImageLinks.scss';
 
-export default function ImageLinks({ sectionClass, wave, titles, links, src, alt }) {
-  const waveSection = `${sectionClass} ${wave ? `waveBg` : ''}`;
-
-  return (
-    <section className={waveSection}>
-      <div className={`${sectionClass}__container`}>
-        <div className={`${sectionClass}__title title-container`}>
-          <Highlight tag="h2">Découvrez nos Deux Activités</Highlight>
-        </div>
-        <div className={`${sectionClass}__content`}>
-          {titles.map((title, index) => (
-
-              <a className={`${sectionClass}__link`} key={index} href={links[index]}>
-                <div className={`${sectionClass}__link--overlay`}></div>
-                <h3 className={`${sectionClass}__link__title`}>{title}</h3>
-                <img className={`${sectionClass}__image`} src={src[index]} alt={alt[index]} width={768} height={350} loading="lazy" />
-              </a>
-
-          ))}
-        </div>
-      </div>
-    </section>
-  )
+export default function ImageLinks({ sectionClass, titles, links, src, alt }) {
+	return (
+		<section className={sectionClass}>
+			<div className={`${sectionClass}__container`}>
+				<div className={`${sectionClass}__title title-container`}>
+					<Highlight tag="h2">Découvrez nos Deux Activités</Highlight>
+				</div>
+				<div className={`${sectionClass}__content`}>
+					{titles.map((title, index) => (
+						<a
+							className={`${sectionClass}__link`}
+							key={index}
+							href={links[index]}
+						>
+							<div
+								className={`${sectionClass}__link--overlay`}
+							></div>
+							<h3 className={`${sectionClass}__link__title`}>
+								{title}
+							</h3>
+							<img
+								className={`${sectionClass}__image`}
+								src={src[index]}
+								alt={alt[index]}
+								width={768}
+								height={350}
+								loading="lazy"
+							/>
+						</a>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
--- frontend/src/components/TextAndImage/TextAndImage.jsx
+++ frontend/src/components/TextAndImage/TextAndImage.jsx
@@ -1,25 +1,41 @@
-import Highlight from "../Highlight/Highlight";
-import "./TextAndImage.scss";
+import Highlight from '../Highlight/Highlight';
+import './TextAndImage.scss';
 
-export default function TextAndImage({ sectionClass, title, text, src, alt, reverse, wave }) {
+export default function TextAndImage({
+	sectionClass,
+	title,
+	text,
+	src,
+	alt,
+	reverse,
+}) {
+	const reverseSection = `${sectionClass} ${
+		reverse ? `${sectionClass}--reverse` : ''
+	}`;
+	const alignEndTitle = `${sectionClass}__title ${
+		reverse ? `${sectionClass}__title--align-end` : ''
+	}`;
 
-    const reverseSection = `${sectionClass} ${reverse ? `${sectionClass}--reverse` : ''}`;
-    const alignEndTitle = `${sectionClass}__title ${reverse ? `${sectionClass}__title--align-end` : ''}`;
-    const waveSection = `${sectionClass} ${wave ? `waveBg` : ''}`;
-
-    return (
-        <section className={`${waveSection}`}>
-            <div className={`${reverseSection} ${sectionClass}__container`}>
-                <div className={`${sectionClass}__content`}>
-                    <div className={`${alignEndTitle} title-container`}>
-                        <Highlight tag="h2">{title}</Highlight>
-                    </div>
-                    <p className={`${sectionClass}__text`}>{text}</p>
-                </div>
-                <div className={`${sectionClass}__wrapper`}>
-                    <img className={`${sectionClass}__illustration`} src={src} alt={alt} width={400} height={400} loading="lazy"/>
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section className={`${reverseSection}`}>
+			<div className={`${reverseSection} ${sectionClass}__container`}>
+				<div className={`${sectionClass}__content`}>
+					<div className={`${alignEndTitle} title-container`}>
+						<Highlight tag="h2">{title}</Highlight>
+					</div>
+					<p className={`${sectionClass}__text`}>{text}</p>
+				</div>
+				<div className={`${sectionClass}__wrapper`}>
+					<img
+						className={`${sectionClass}__illustration`}
+						src={src}
+						alt={alt}
+						width={400}
+						height={400}
+						loading="lazy"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }

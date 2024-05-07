ea53f88fe38cf61c87711a067927a84aab08af24
Naoh <27915369+N40h@users.noreply.github.com>
2024-05-07 13:51:25 +0200

    feat(News, CheeseGallery): Ajout du composant de la Modal
    Ajout du composant de la Modal pour permettre la modification des articles et des fromages

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,10 +1,29 @@
 import './Card.scss';
 
-export default function Card({ src, alt, caption, description, width, height, loading }) {
-  return (
-    <figure className="card">
-      <img src={src} alt={alt} width={width} height={height} loading={loading} />
-      <div className="card__description"><figcaption>{caption}</figcaption>{description}</div>
-    </figure>
-  );
+export default function Card({
+	src,
+	alt,
+	caption,
+	description,
+	width,
+	height,
+	loading,
+	modifyIcon,
+}) {
+	return (
+		<figure className="card">
+			<img
+				src={src}
+				alt={alt}
+				width={width}
+				height={height}
+				loading={loading}
+			/>
+			<div className="card__description">
+				<figcaption>{caption}</figcaption>
+				{description}
+			</div>
+			<div className="card__icon">{modifyIcon}</div>
+		</figure>
+	);
 }
---
+++ frontend/src/components/Modal/Modal.jsx
@@ -0,0 +1,98 @@
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import './Modal.scss';
+
+export default function Modal({
+	closeModal,
+	selectedArticle,
+	selectedCheese,
+	handleUpdate,
+}) {
+	const selectedItem = selectedArticle || selectedCheese;
+	const [updatedItem, setUpdatedItem] = useState(selectedItem);
+
+	const handleChange = (e) => {
+		setUpdatedItem({
+			...updatedItem,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		handleUpdate(updatedItem);
+		closeModal();
+	};
+
+	if (!selectedItem) return null;
+	console.log(selectedItem);
+
+	return (
+		<>
+			<div className="modal">
+				<div className="modal__overlay"></div>
+				<div className="modal__content">
+					<div className="modal__content__closing">
+						<FontAwesomeIcon
+							icon={faXmark}
+							onClick={closeModal}
+							className="modal__content__closing--btn"
+						/>
+					</div>
+					<div className="modal__content__formContainer">
+						<h2 className="modal__content__formContainer--title">
+							Modifier l'article
+						</h2>
+						<form
+							className="modal__content__formContainer--form"
+							onSubmit={handleSubmit}
+						>
+							<div className="modal__content__formContainer--inputControl">
+								<label htmlFor="title">Titre</label>
+								<input
+									type="text"
+									name="title"
+									id="title"
+									value={
+										selectedItem.caption ||
+										selectedItem.title
+									}
+									placeholder="Nouveau titre"
+									onChange={handleChange}
+								/>
+							</div>
+							{selectedItem.date && (
+								<div className="modal__content__formContainer--inputControl">
+									<label htmlFor="date">Date</label>
+									<input
+										type="text"
+										name="date"
+										id="date"
+										value={selectedItem.date}
+										onChange={handleChange}
+										placeholder="Nouvelle date"
+									/>
+								</div>
+							)}
+							<div className="modal__content__formContainer--inputControl">
+								<label htmlFor="content">Contenu</label>
+								<textarea
+									name="content"
+									id="content"
+									value={
+										selectedItem.description ||
+										selectedItem.content
+									}
+									onChange={handleChange}
+									placeholder="Nouveau contenu"
+								></textarea>
+							</div>
+							<button type="submit">Modifier</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
